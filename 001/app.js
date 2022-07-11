7c97e4c00a567c19705bcf19dc007eaf7bab8969
A-Kija <arvydas.kijakauskas@gmail.com>
2022-07-11 22:03:01 +0300

    map filter sort

--- 001/app.js
+++ 001/app.js
@@ -15,20 +15,34 @@ console.log('start', rand(1, 1000));
 //     mas.push(rand(10, 99));
 // }
 
-const mas = [...Array(10)].map(_ => rand(10, 99));
+const mas = [...Array(20)].map(_ => rand(10, 99));
 
 
 
-mas.sort((a, b) => {
-    if (b > a) return -1;
-    if (b < a) return 1;
-    return 0;
-});
+// mas.sort((a, b) => {
+//     if (b > a) return -1;
+//     if (b < a) return 1;
+//     return 0;
+// });
 
 
-mas.forEach(e => console.log(e));
+// const fr = mas.forEach(e => console.log(e));
+// const map = mas.map(e => e + 10);
+
+// const mas2 = mas;
 
 
-console.table(mas);
+// console.log(fr);
+// console.log(map);
+
+// map[0] = 'bla';
+
+// const fmas = mas.filter(e => e <= 50);
+
+const fmas = mas.filter(e => e > 66).map(e => e * 3).sort((a, b) => a % 2 - b % 2 || a - b);
+
+console.log(fmas);
+
+
 
 
