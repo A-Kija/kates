f6573a777bb2e8e28d29469a4923d37653c90253
A-Kija <arvydas.kijakauskas@gmail.com>
2022-07-12 22:00:52 +0300

    objektai

--- 002/app.js
+++ 002/app.js
@@ -21,17 +21,38 @@ const data = cats.map((n, i) => ({name: n, id: i + 1}));
 
 // data.sort((a, b) => a.name == b.name ? 0 : a.name > b.name ? 1 : -1);
 
-data.sort((a, b) => {
-    if (a.name > b.name) return 1;
-    if (a.name < b.name) return -1;
-    return 0;
-});
+// data.sort((a, b) => {
+//     if (a.name > b.name) return 1;
+//     if (a.name < b.name) return -1;
+//     return 0;
+// });
 
 
 const randId = rand(1, 8);
-console.log(randId);
+// console.log(randId);
 
-const dataDel = data.filter(c => c.id != randId);
+// const dataDel = data.filter(c => c.id != randId);
 
+// const dataDel = data.filter(c => c.id == randId)[0];
 
-console.log(dataDel);
+// data.sort(_ => rand(0, 1) ? 1 : -1);
+
+// const half = data.filter(c => !(c.id % 2)).sort(_ => rand(0, 1) ? 1 : -1);
+
+// const mod = data.map(e => !(e.id % 2) ? half.shift() : e);
+
+// const mod = data.map(c => ({...c, weight: rand(1, 7), name: 'Rainius'}) );
+
+// {...c} === {id: c.id, name: c.name, weight: rand(1, 7)}
+
+const mod1 = data.map(c => ({...c, weight: rand(1, 7), [c.name]:42, ['have-fun']: true  }));
+
+// const mod1 = data.map(c => {
+//     c.weight = rand(1, 7);
+//     c[c.name] = 42
+//     return c;
+// }).sort(_ => rand(0, 1) ? 1 : -1);
+
+// mod[0].weight = 100;
+
+console.log(mod1);
