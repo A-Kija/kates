51c77e00770f011ccdf669399cef7d4950497db2
A-Kija <arvydas.kijakauskas@gmail.com>
2022-07-13 21:11:27 +0300

    list ir keys

--- r1/src/App.jsx
+++ r1/src/App.jsx
@@ -2,6 +2,8 @@ import './App.css';
 import Bebras from './Components/003/Bebras';
 import Briedis from './Components/003/Briedis';
 
+const cats = ['Pilkis', 'Mulkis', 'Kriukis'];
+
 function App() {
 
   return (
@@ -10,6 +12,9 @@ function App() {
        <h1 className="red">Start</h1>
        <Bebras></Bebras>
        <Briedis></Briedis>
+       {
+        cats.map((c, i) => <span key={i}>{c}</span>)
+       }
       </header>
     </div>
   );
