ebcff377157d93559f39513b5802ffd860a60417
A-Kija <arvydas.kijakauskas@gmail.com>
2022-07-13 20:44:48 +0300

    style

--- r1/src/Components/003/Briedis.jsx
+++ r1/src/Components/003/Briedis.jsx
@@ -1,9 +1,13 @@
+import rand from "../../Functions/rand";
+
 function Briedis() {
 
     return (
     
     <>
-        <h2>Hello, Briedis!</h2>
+        <h2 style={{
+            fontSize: rand(15, 55) + 'px'
+        }}>Hello, Briedis!</h2>
         <h3 style={{color: 'green'}}>Bey, bye</h3>
     </>
 
