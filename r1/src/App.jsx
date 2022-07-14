20b70f2b83b195327dc97e73d270fc897fffdc3e
A-Kija <arvydas.kijakauskas@gmail.com>
2022-07-14 20:43:22 +0300

    10 circles

--- r1/src/App.jsx
+++ r1/src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css';
+import Circle from './Components/004/Circle';
 import SC1 from './Components/004/SC1';
 import SQ100 from './Components/004/SQ100';
 import Text from './Components/004/Text';
@@ -10,7 +11,7 @@ function App() {
         <div className="App">
             <header className="App-header">
                 <h1>Props</h1>
-                <Text zodis="Geras Zuikis" zenklas="!" spalva="green"></Text>
+                {/* <Text zodis="Geras Zuikis" zenklas="!" spalva="green"></Text>
                 <Text zodis="Blogas Zuikis" zenklas="%" spalva="crimson"></Text>
                 <Text zodis="Raudonas Zuikis" zenklas="$" spalva="yellow"></Text>
                 <SC1 form="circle"></SC1>
@@ -19,7 +20,12 @@ function App() {
                 <SQ100 h={55} w={700}></SQ100>
                 <SQ100 h={800} w={20}></SQ100>
                 <TwoColor c1="yellow" c2="black"></TwoColor>
-                <TwoColor c1="yellow" c2="yellow"></TwoColor>
+                <TwoColor c1="yellow" c2="yellow"></TwoColor> */}
+                <div className="container">
+                    {
+                        [...Array(10)].map((_, i) => <Circle key={i} number={i + 1}></Circle>)
+                    }
+                </div>
             </header>
         </div>
     );
---
+++ r1/src/Components/004/Circle.jsx
@@ -0,0 +1,8 @@
+function Circle({number}) {
+
+    return (
+    <div className="circle">{number}</div>
+    );
+}
+
+export default Circle;
