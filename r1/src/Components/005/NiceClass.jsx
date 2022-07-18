14c63e55e336cbfac59bd59a3bf6bc40ed611a9e
A-Kija <arvydas.kijakauskas@gmail.com>
2022-07-18 21:35:26 +0300

    kv

--- r1/src/Components/005/NiceClass.jsx
+++ r1/src/Components/005/NiceClass.jsx
@@ -4,7 +4,7 @@ class NiceClass extends Component {
 
     constructor() {
         super();
-        this.state = { dydis: 30, fonas: 'skyblue', count: 1}; // default
+        this.state = { dydis: 30, fonas: 'skyblue', count: 1, kv: []}; // default
     }
 
     addCount = () => {
@@ -22,11 +22,20 @@ class NiceClass extends Component {
         this.setState(this.state.fonas === 'orange' ? {fonas: 'skyblue'} : {fonas: 'orange'});
     }
 
+    addKv = () => {
+        this.setState(s => ({kv: [...s.kv, '']}));
+    }
+
 
     render() {
         return (
             <>
             <h2>{this.state.count}</h2>
+            <div className="container">
+            {
+                this.state.kv.map((_, i) => <div className="sc" key={i}></div>)
+            }
+            </div>
             <h3 style={{
                 color: this.props.spalva,
                 backgroundColor: this.state.fonas,
@@ -35,6 +44,7 @@ class NiceClass extends Component {
             <button className="green" onClick={this.greenButtonClicked}>more</button>
             <button className="blue" onClick={() => this.blueButtonClicked()}>color</button>
             <button className="red" onClick={this.addCount}>+1</button>
+            <button className="red" onClick={this.addKv}>+1 KV</button>
             </>
         );
     }
