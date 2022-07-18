072096ff1c0ec5bdb15c7d4372ec1365d379a4ae
A-Kija <arvydas.kijakauskas@gmail.com>
2022-07-18 21:03:56 +0300

    +1

--- r1/src/Components/005/NiceClass.jsx
+++ r1/src/Components/005/NiceClass.jsx
@@ -4,19 +4,36 @@ class NiceClass extends Component {
 
     constructor() {
         super();
-        this.state = { dydis: 30, fonas: 'skyblue'}; // default
+        this.state = { dydis: 30, fonas: 'skyblue', count: 1}; // default
+    }
+
+    addCount = () => {
+        this.setState({count: this.state.count + 1})
+    }
+
+    greenButtonClicked = () => {
+        this.setState({dydis: 60});
+
+        console.log(this.state);
+    }
+
+    blueButtonClicked() {
+        this.setState(this.state.fonas === 'orange' ? {fonas: 'skyblue'} : {fonas: 'orange'});
     }
 
 
     render() {
         return (
             <>
+            <h2>{this.state.count}</h2>
             <h3 style={{
                 color: this.props.spalva,
                 backgroundColor: this.state.fonas,
                 fontSize: this.state.dydis + 'px'
             }}>Aš princesė</h3>
-            <button className="green">more</button>
+            <button className="green" onClick={this.greenButtonClicked}>more</button>
+            <button className="blue" onClick={() => this.blueButtonClicked()}>color</button>
+            <button className="red" onClick={this.addCount}>+1</button>
             </>
         );
     }
