bb125e030d57193ab8d2256e2bf086943195356d
A-Kija <arvydas.kijakauskas@gmail.com>
2022-07-19 19:37:03 +0300

    sq1

--- r1/src/Components/006/NiceAndFun.jsx
+++ r1/src/Components/006/NiceAndFun.jsx
@@ -7,6 +7,7 @@ function NiceAndFun({ spalva }) {
     const [bg, setBg] = useState('white'); //Hook
 
     const [count, setCount] = useState(0);
+    const [sq1, setSq1] = useState([]);
 
     const doSize = () => {
         setSize(s => s === 56 ? 36 : 56);
@@ -20,6 +21,10 @@ function NiceAndFun({ spalva }) {
         setCount(s => s + rand(5, 10));
     }
 
+    const doSq1 = () => {
+        setSq1(s => [...s, '']);
+    }
+
     return (
         <>
             <h2 style={
@@ -30,9 +35,15 @@ function NiceAndFun({ spalva }) {
                 }
             }>Braškė {count}</h2>
             <div className="container">
+                {
+                    sq1.map((_, i) => <div className="sc" key={i}>{i + 1}</div>)
+                }
+            </div>
+            <div className="container">
             <button onClick={doSize}>SIZE</button>
             <button onClick={doBack}>BG</button>
             <button onClick={doMore}>MORE</button>
+            <button className="blue" onClick={doSq1}>+SQ1</button>
             </div>
         </>
     )
