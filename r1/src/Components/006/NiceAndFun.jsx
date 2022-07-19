b8495092dac8e29b0360fd0ff32cf500d39a2156
A-Kija <arvydas.kijakauskas@gmail.com>
2022-07-19 20:02:14 +0300

    reset 1

--- r1/src/Components/006/NiceAndFun.jsx
+++ r1/src/Components/006/NiceAndFun.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import rand from '../../Functions/rand';
+import randColor from '../../Functions/randColor';
 
 function NiceAndFun({ spalva }) {
 
@@ -8,6 +9,7 @@ function NiceAndFun({ spalva }) {
 
     const [count, setCount] = useState(0);
     const [sq1, setSq1] = useState([]);
+    const [sq2, setSq2] = useState([]);
 
     const doSize = () => {
         setSize(s => s === 56 ? 36 : 56);
@@ -25,6 +27,10 @@ function NiceAndFun({ spalva }) {
         setSq1(s => [...s, '']);
     }
 
+    const doSq2 = () => {
+        setSq2(s => [...s, randColor()]);
+    }
+
     return (
         <>
             <h2 style={
@@ -40,10 +46,17 @@ function NiceAndFun({ spalva }) {
                 }
             </div>
             <div className="container">
+                {
+                    sq2.map((c, i) => <div className="sc" key={i} style={{backgroundColor: c}}></div>)
+                }
+            </div>
+            <div className="container">
             <button onClick={doSize}>SIZE</button>
             <button onClick={doBack}>BG</button>
             <button onClick={doMore}>MORE</button>
             <button className="blue" onClick={doSq1}>+SQ1</button>
+            <button className="blue" onClick={doSq2}>+SQ2</button>
+            <button className="red" onClick={() => setSq1([])}>reset SQ1</button>
             </div>
         </>
     )
