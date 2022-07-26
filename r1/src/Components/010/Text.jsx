5adc942a56ac8160771d92c9e7df04093b4bca4f
A-Kija <arvydas.kijakauskas@gmail.com>
2022-07-26 20:19:28 +0300

    color style

--- r1/src/Components/010/Text.jsx
+++ r1/src/Components/010/Text.jsx
@@ -2,17 +2,22 @@ import { useState } from 'react';
 function Text() {
 
     const [text, setText] = useState('');
+    const [color, setColor] = useState('#009900');
 
     const inputHandler = e => {
         setText(e.target.value)
     }
 
+    const colorHandler = e => {
+        setColor(e.target.value)
+    }
+
     return (
         <fieldset>
-            <legend>{text ? text : 'text'}</legend>
+            <legend style={{color}}>{text ? text : 'text'}</legend>
             <input type="text" value={text} onChange={inputHandler}></input>
             <div></div>
-            <input type="color"></input>
+            <input type="color" value={color} onChange={colorHandler}></input>
         </fieldset>
     );
 }
