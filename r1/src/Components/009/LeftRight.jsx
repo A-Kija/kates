80ac5eec85459312d37760239f778e7b23e45400
A-Kija <arvydas.kijakauskas@gmail.com>
2022-07-26 19:12:03 +0300

    show

--- r1/src/Components/009/LeftRight.jsx
+++ r1/src/Components/009/LeftRight.jsx
@@ -24,14 +24,14 @@ function LeftRight({data}) {
 
 
     useEffect(()=> {
-        setNumbers(data.map((e, i) => ({...e, row: i})).filter(d => d.left !== d.right));
+        setNumbers(data.map((e, i) => ({...e, row: i, show: true})).filter(d => d.left !== d.right));
     }, [data]);
 
     const add = () => {
         const left = rand(0, 45);
         const right = rand(0, 45);
         if (left !== right) {
-            setNumbers(n => [...n, {left, right, row: n.length}]);
+            setNumbers(n => [...n, {left, right, row: n.length, show: true}]);
         }
     }
 
@@ -46,12 +46,24 @@ function LeftRight({data}) {
     const doDefault = () => {
         setNumbers(n => [...n].sort((a, b) => a.row - b.row));
     }
+
+    const filter20 = () => {
+        setNumbers(n => n.map(nb => ({...nb, show: nb.left > 20})));
+    }
     
+    const filter10 = () => {
+        setNumbers(n => n.map(nb => ({...nb, show: nb.left < 10})));
+    }
+
+    const nofilter = () => {
+        setNumbers(n => n.map(nb => ({...nb, show: true})));
+    }
+
     return (
         <>
         <ul>
             {
-                numbers?.map((lr, i) => <li key={i}>{lr.left}-{lr.right}</li>)
+                numbers?.map((lr, i) => lr.show ? <li key={i}>{lr.left}-{lr.right}</li> : null)
             }
         </ul>
         <div className="container">
@@ -63,6 +75,10 @@ function LeftRight({data}) {
             <button className="red" onClick={() => setSortOrder('left')}>Left Sort</button>
             <button className="red" onClick={() => setSortOrder('right')}>Right Sort</button>
             <button className="red" onClick={() => setSortOrder('default')}>Default Sort</button>
+
+            <button className="green" onClick={filter20}>min 21</button>
+            <button className="green" onClick={filter10}>max 9</button>
+            <button className="green" onClick={nofilter}>Show All</button>
         </div>
         </>
     )
