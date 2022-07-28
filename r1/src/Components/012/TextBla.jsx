a6dc5a24bff7391bc11f8647177cb5756d2f6767
A-Kija <arvydas.kijakauskas@gmail.com>
2022-07-28 18:48:31 +0300

    color

--- r1/src/Components/012/TextBla.jsx
+++ r1/src/Components/012/TextBla.jsx
@@ -4,13 +4,22 @@ function TextBla() {
 
     const [word, setWord] = useState('');
     const [text, setText] = useState('');
+    const [color, setColor] = useState('');
+    const [wordColor, setWordColor] = useState('');
+
+    const doIt = () => {
+        setWord(text);
+        setWordColor(color);
+    }
+
 
     return (
-        <>
-        <h2>{word}</h2>
+        <div className="text-bin">
+        <h2 style={{color: wordColor}}>{word}</h2>
         <input type="text" value={text} onChange={e => setText(e.target.value)} />
-        <button onClick={() => setWord(text)}>Do</button>
-        </>
+        <input type="color" value={color} onChange={e => setColor(e.target.value)} />
+        <button onClick={doIt}>Do</button>
+        </div>
     )
 }
 
