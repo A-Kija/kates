a42e3fc761eae0cd1ba3a9a2538b8a098ddbb2c0
A-Kija <arvydas.kijakauskas@gmail.com>
2022-07-28 20:44:10 +0300

    cb2

--- r1/src/Components/012/BlueRed.jsx
+++ r1/src/Components/012/BlueRed.jsx
@@ -3,15 +3,17 @@ import { useState } from "react"
 function BlueRed() {
 
     const [cb, setCb] = useState(true);
+    const [cb2, setCb2] = useState(true);
 
     return (
         <>
         <div className="nice-square" style={{
             backgroundColor: cb ? null : '#fb618a60',
             borderColor: cb ? null : '#fb618a',
+            borderRadius: cb2 ? null : '50%',
         }}></div>
         <input type="checkbox" checked={cb} onChange={() => setCb(b => !b)}></input>
-        
+        <input type="checkbox" checked={cb2} onChange={() => setCb2(b => !b)}></input>
         </>
     )
 }
