d38bdeee5b49f32dc4020466adeaa57b9ed45244
A-Kija <arvydas.kijakauskas@gmail.com>
2022-08-08 19:14:46 +0300

    w/ sort

--- r1/src/Components/017/Users.jsx
+++ r1/src/Components/017/Users.jsx
@@ -7,21 +7,33 @@ function Users() {
     const [users, setUsers] = useState(null);
 
     useEffect(() => {
-
         axios.get('https://jsonplaceholder.typicode.com/users')
         .then(res => setUsers(res.data));
-
-
     }, []);
+
+    const sortName = () => {
+        setUsers(u => [...u].sort((a, b) => a.name.localeCompare(b.name)))
+    }
+
+    const sortDefault = () => {
+        setUsers(u => [...u].sort((a, b) => a.id - b.id))
+    }
 
 
     return (
         <>
             <ul>
                 {
-                    users ? users.map(u => <li key={u.id}>{u.name}</li>) : <li className="loader"></li>
+                    users ? users.map(u => (<li className="users-list" key={u.id}>
+                        <b>{u.name}</b>
+                        <i>{u.address.city}</i>
+                        <i>{u.company.name}</i>
+                        </li>))
+                     : <li className="loader"></li>
                 }
             </ul>
+            <button onClick={sortName}>Sort Name</button>
+            <button onClick={sortDefault}>Default Sort</button>
         </>
     )
 
