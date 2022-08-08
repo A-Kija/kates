53afbdcb96af05a2939633f75fe2f21f3c2d6b28
A-Kija <arvydas.kijakauskas@gmail.com>
2022-08-08 20:49:54 +0300

    selektorius

--- r1/src/Components/017/Posts.jsx
+++ r1/src/Components/017/Posts.jsx
@@ -2,16 +2,27 @@ import { useEffect } from "react";
 import { useState } from "react";
 import axios from 'axios';
 
+const perPage = 30;
+
 function Posts() {
 
     const [posts, setPosts] = useState(null);
     const [users, setUsers] = useState(null);
+
+    const [pages, setPages] = useState(0);
 
     useEffect(() => {
         axios.get('https://jsonplaceholder.typicode.com/posts')
         .then(res => setPosts(res.data));
     }, []);
 
+    useEffect(() => {
+        if (null === posts) {
+            return;
+        }
+        setPages(Math.ceil(posts.length / perPage));
+    }, [posts]);
+    
    
 
     useEffect(() => {
@@ -19,10 +30,21 @@ function Posts() {
         .then(res => setUsers(res.data));
     }, []);
 
+    const sortTitle = () => {
+        setPosts(p => [...p].sort((a, b) => b.title.length - a.title.length))
+    }
+
 
 
     return (
         <>
+            <div className="container">
+            <select>
+                {
+                    [...Array(pages)].map((_, i) => <option key={i}>{i + 1} Page</option>)
+                }
+            </select>
+            </div>
             <ul>
                 {
                     posts ? posts.map(p => (<li className="users-list" key={p.id}>
@@ -32,6 +54,7 @@ function Posts() {
                      : <li className="loader"></li>
                 }
             </ul>
+            <button onClick={sortTitle}>Sort Title</button>
         </>
     )
 
