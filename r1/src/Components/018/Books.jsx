17d86dd0b9962f6fc10989b5578e7a0e5d8ffe72
A-Kija <arvydas.kijakauskas@gmail.com>
2022-08-09 21:51:36 +0300

    sort

--- r1/src/Components/018/Books.jsx
+++ r1/src/Components/018/Books.jsx
@@ -8,19 +8,21 @@ function Books() {
     const [books, setBooks] = useState(null);
     const [types, setTypes] = useState(null);
     const [cat, setCat] = useState(0);
+    const [sort, setSort] = useState(0);
 
     useEffect(() => {
         axios.get('https://in3.dev/knygos/')
-        .then(res => setBooks(res.data.map(b => ({
-            ...b,
-            color: randColor(),
-            show: true
-        }))));
+            .then(res => setBooks(res.data.map((b, i) => ({
+                ...b,
+                color: randColor(),
+                show: true,
+                row: i
+            }))));
     }, []);
 
     useEffect(() => {
         axios.get('https://in3.dev/knygos/types/')
-        .then(res => setTypes(res.data));
+            .then(res => setTypes(res.data));
     }, []);
 
     useEffect(() => {
@@ -29,30 +31,53 @@ function Books() {
             show: (cat === 0 || cat === book.type) ? true : false
         })))
 
-    }, [cat])
+    }, [cat]);
+
+    useEffect(() => {
+        switch (sort) {
+            case 0:
+                setBooks(b => b ? [...b].sort((a, b) => a.row - b.row) : null);
+                break;
+            case 1:
+                setBooks(b => b ? [...b].sort((a, b) => a.price - b.price) : null);
+                break;
+            case 2:
+                setBooks(b => b ? [...b].sort((a, b) => b.price - a.price) : null);
+                break;
+            default:
+        }
+    }, [sort]);
 
     return (
         <>
-        { types ?
-        <div className="container">
-            <select value={cat} onChange={e => setCat(parseInt(e.target.value))}>
-                <option value="0">Visos</option>
-                {
-                    types?.map(t => <option key={t.id} value={t.id}>{t.title}</option>)
-                }
-            </select>
-        </div> : null
-        }
-
-        <ul className="books-list">
-            {
-                books ? books.map(b => 
-                    b.show ?
-                    <Book key={b.id} book={b} cat={types?.find(t => t.id === b.type).title ?? '...loading'} />
-                    : null
-                ) : <li className="loader"></li>
+            {types ?
+                <div className="container">
+                    <select value={cat} onChange={e => setCat(parseInt(e.target.value))}>
+                        <option value="0">Visos</option>
+                        {
+                            types?.map(t => <option key={t.id} value={t.id}>{t.title}</option>)
+                        }
+                    </select>
+                </div> : null
             }
-        </ul>
+            <div className="container">
+                <select value={sort} onChange={e => setSort(parseInt(e.target.value))}>
+                    <option value="0">Pradinis</option>
+                    <option value="1">Kaina min-max</option>
+                    <option value="2">Kaina max-min</option>
+                </select>
+            </div>
+
+
+            <ul className="books-list">
+                {
+                    books ? books.map(b =>
+                        b.show ?
+                            <Book key={b.id} book={b} cat={types?.find(t => t.id === b.type).title ?? '...loading'} />
+                            : null
+                    ) : <li className="loader"></li>
+                }
+            </ul>
         </>
     )
 
