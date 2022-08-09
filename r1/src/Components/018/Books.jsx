fb3e2b0d9863cb95d0875e0dae9a81197af37cd9
A-Kija <arvydas.kijakauskas@gmail.com>
2022-08-09 21:21:28 +0300

    filter

--- r1/src/Components/018/Books.jsx
+++ r1/src/Components/018/Books.jsx
@@ -7,10 +7,15 @@ function Books() {
 
     const [books, setBooks] = useState(null);
     const [types, setTypes] = useState(null);
+    const [cat, setCat] = useState(0);
 
     useEffect(() => {
         axios.get('https://in3.dev/knygos/')
-        .then(res => setBooks(res.data.map(b => ({...b, color: randColor()}))));
+        .then(res => setBooks(res.data.map(b => ({
+            ...b,
+            color: randColor(),
+            show: true
+        }))));
     }, []);
 
     useEffect(() => {
@@ -18,11 +23,19 @@ function Books() {
         .then(res => setTypes(res.data));
     }, []);
 
+    useEffect(() => {
+        setBooks(b => b?.map(book => ({
+            ...book,
+            show: (cat === 0 || cat === book.type) ? true : false
+        })))
+
+    }, [cat])
+
     return (
         <>
         { types ?
-        <div class="container">
-            <select>
+        <div className="container">
+            <select value={cat} onChange={e => setCat(parseInt(e.target.value))}>
                 <option value="0">Visos</option>
                 {
                     types?.map(t => <option key={t.id} value={t.id}>{t.title}</option>)
@@ -33,7 +46,11 @@ function Books() {
 
         <ul className="books-list">
             {
-                books ? books.map(b => <Book key={b.id} book={b} cat={types?.find(t => t.id === b.id).title ?? '...loading'} />) : <li className="loader"></li>
+                books ? books.map(b => 
+                    b.show ?
+                    <Book key={b.id} book={b} cat={types?.find(t => t.id === b.type).title ?? '...loading'} />
+                    : null
+                ) : <li className="loader"></li>
             }
         </ul>
         </>
