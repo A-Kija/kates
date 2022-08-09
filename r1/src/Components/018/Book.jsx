af2851393b3919fa3b3b4514fcd39404864b5fd8
A-Kija <arvydas.kijakauskas@gmail.com>
2022-08-09 20:02:27 +0300

    types

--- r1/src/Components/018/Book.jsx
+++ r1/src/Components/018/Book.jsx
@@ -1,11 +1,12 @@
-import randColor from '../../Functions/randColor';
 
-function Book({book}) {
+
+function Book({book, cat}) {
 
     
 
     return (
-        <li className="li-book" style={{backgroundColor: randColor()}}>
+        <li className="li-book" style={{backgroundColor: book.color + '40'}}>
+            <div className="cat">{cat}</div>
             <h2>{book.title}</h2>
             <img src={book.img} alt={book.title} />
             <div className="author">{book.author}</div>
--- r1/src/Components/018/Books.jsx
+++ r1/src/Components/018/Books.jsx
@@ -1,20 +1,27 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import Book from './Book';
+import randColor from '../../Functions/randColor';
 
 function Books() {
 
     const [books, setBooks] = useState(null);
+    const [types, setTypes] = useState(null);
 
     useEffect(() => {
         axios.get('https://in3.dev/knygos/')
-        .then(res => setBooks(res.data));
+        .then(res => setBooks(res.data.map(b => ({...b, color: randColor()}))));
+    }, []);
+
+    useEffect(() => {
+        axios.get('https://in3.dev/knygos/types/')
+        .then(res => setTypes(res.data));
     }, []);
 
     return (
         <ul className="books-list">
             {
-                books ? books.map(b => <Book key={b.id} book={b} />) : <li className="loader"></li>
+                books ? books.map(b => <Book key={b.id} book={b} cat={types?.find(t => t.id === b.id).title ?? '...loading'} />) : <li className="loader"></li>
             }
         </ul>
     )
